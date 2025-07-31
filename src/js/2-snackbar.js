d2d5e4864991ccd4447e4b2d614056550a5d4b4f
Svitlana <svetlanayurevna98@gmail.com>
2025-07-31 23:56:42 +0300

    task 1 and 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,51 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        titleColor: '#fff',
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: '#fff',
+        messageSize: '16px',
+        backgroundColor: '#59a10d',
+        position: 'topRight',
+        iconUrl: '../img/ok-img.png',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        titleColor: '#fff',
+        message: `Rejected promise in ${delay}ms`,
+        messageColor: '#fff',
+        messageSize: '16px',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+        iconUrl: '../img/warning-img.png',
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
